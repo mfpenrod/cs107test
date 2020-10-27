0f5e2c358d5b47d9c902f3d2c7a8205928cfcdc4
Mark Penrod <mark.penrod@gmail.com>
2020-10-26 23:10:29 -0400

    Updated roots and test_roots

--- roots.py
+++ roots.py
@@ -57,29 +57,3 @@ def quad_roots(a=1.0, b=2.0, c=0.0):
         r2 = -b - sqrtdisc
         twoa = 2.0 * a
         return (r1 / twoa, r2 / twoa)
-
-# test_roots.py
-import pytest
-import roots
-
-def test_quadroots_result():
-    assert roots.quad_roots(1.0, 1.0, -12.0) == ((3+0j), (-4+0j))
-
-def test_quadroots_types():
-    with pytest.raises(TypeError):
-        roots.quad_roots("", "green", "hi")
-
-def test_quadroots_zerocoeff():
-    with pytest.raises(ValueError):
-        roots.quad_roots(a=0.0)
-
-def test_linearoots_result():
-    assert roots.linear_roots(2.0, -3.0) == 1.5
-
-def test_linearroots_types():
-    with pytest.raises(TypeError):
-        roots.linear_roots("ocean", 6.0)
-
-def test_linearroots_zerocoeff():
-    with pytest.raises(ValueError):
-        roots.linear_roots(a=0.0)
